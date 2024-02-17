81473f149a07cda723c0a5385cb2c6d71cdf2dcc
Maksim Shakavin <maksim.shakavin@sap.com>
2024-02-17 21:29:14 +0100

    Initial commit :rocket:

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+  packages = [
+   pkgs.age
+   pkgs.cloudflared
+   pkgs.fluxcd
+   pkgs.helmfile
+   pkgs.jq
+   pkgs.kubeconform
+   pkgs.kustomize
+   pkgs.moreutils
+   pkgs.sops
+   pkgs.stern
+   pkgs.yq-go
+  ];
+}
